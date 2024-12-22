4cad7ed9152f8970a3ec9d8d125eafb9cf8b97aa
Prateek Patidar <patidarprateek2593@gmail.com>
2024-12-22 22:11:43 +0530

    setting up router and starting server

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/Prateek2593/students-api/internal/config"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	//load config
+	cfg := config.MustLoad()
+	//database setup
+	//set up router
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to students-api"))
+	})
+	//setup server
+	server := http.Server{
+		Addr:    cfg.HTTPServer.Addr,
+		Handler: router,
+	}
+
+	fmt.Printf("Server started %s", cfg.HTTPServer.Addr)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("Failed to start server")
+	}
 }
